02e834918a5cb724960a343bac211590291a2119
GreerPage <greerishere@gmail.com>
2019-12-02 17:47:19 -0500

    made better

--- morse.py
+++ morse.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-#import morseKey1
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -11,14 +10,12 @@ def dot():
 	print('.', end = '')
 	time.sleep(0.2)
 	GPIO.output(18, False)
-	#print(' ', end = '')
 	time.sleep(0.1)
 def dash():
 	 GPIO.output(18, True)
 	 print('-', end = '')
 	 time.sleep(0.6)
 	 GPIO.output(18, False)
-	 #print(' ', end = '')
 	 time.sleep(0.1)
 def space():
 	GPIO.output(18, False)
@@ -28,4 +25,3 @@ def letterspace():
 	GPIO.output(18, False)
 	print(' ', end = '')
 	time.sleep(0.6)
-#morseKey1.texttomorse('hello i am greer')
