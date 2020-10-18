2ef5d4ec72152b13d5c68af1d03022f5d33ad01d
GreerPage <greerishere@gmail.com>
2020-10-17 21:50:31 -0400

    fixed spacing issue

--- morse/run.py
+++ morse/run.py
@@ -1,11 +1,12 @@
 from .translate import char_to_morse
-from .lights import letterspace
+from .lights import letterspace, space
 
 def text_to_morse(text):
     t = text.lower().split()
     for word in t:
         for char in word:
             char_to_morse(char)
-        letterspace()
+            letterspace()
+        space()
     print()
         
