7ae089881ccfe5294f74b264de994852f49a4234
GreerPage <greerishere@gmail.com>
2020-10-17 20:45:13 -0400

    updated cli interface

--- morse.py
+++ morse.py
@@ -1,8 +1,50 @@
 import sys
 from morse import text_to_morse
 
-args = ' '.join(sys.argv)
-args = args.replace('morse.py', '')
 
-while True:
-	text_to_morse(args)
+def main(message, loop=False):
+	if loop:
+		while True:
+			text_to_morse(message)
+	else:
+		text_to_morse(message)
+	
+if __name__ == '__main__':
+	args, name = sys.argv, sys.argv[0]
+	args.pop(0)
+
+	if args == []:
+		print('Usage: {} [ MESSAGE ] -l [ TRUE/FALSE ]'.format(name))
+		print('Try \'{} --help\' for more information.'.format(name))
+
+	elif args[0] == '-l' or args[0] == '--loop':
+		print('Usage: {} [ MESSAGE ] -l [ TRUE/FALSE ]'.format(name))
+		print('Try \'{} --help\' for more information.'.format(name))
+	
+	elif '-h' in args or '--help' in args:
+		print('Usage: {} [ MESSAGE ] -l [ TRUE/FALSE ]'.format(name))
+		print('Example: {} go to https://spotify.greerpage.com please -l true')
+		print()
+		print('-l, --loop	if set to true the message will be displayed until the process is killed (false by default)')
+	
+	elif '-l' in args:
+		m = ' '.join(args).split('-l')
+		if m[1].lower() == 'true':
+			main(m[0], True)
+		elif m[1].lower() == 'false':
+			main(m[0], False)
+		else:
+			print('Usage: {} [ MESSAGE ] -l [ TRUE/FALSE ]'.format(name))
+			print('Try \'{} --help\' for more information.'.format(name))
+	
+	elif '--loop' in args:
+		m = ' '.join(args).split('--loop')
+		if m[1].lower() == 'true':
+			main(m[0], True)
+		elif m[1].lower() == 'false':
+			main(m[0], False)
+		else:
+			print('Usage: {} [ MESSAGE ] -l [ TRUE/FALSE ]'.format(name))
+			print('Try \'{} --help\' for more information.'.format(name))
+	else:
+		main(' '.join(args), False)
